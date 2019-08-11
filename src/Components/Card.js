8fb090005f5859be476adc46c636af1ca3bac843
Marco Bartelt <mbartelt@pro-volution.de>
2019-08-11 18:03:22 +0200

    improves a11y and security

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -10,22 +10,28 @@ import Code from './Icons/Code';
 import './Card.scss';
 
 const Card = () => (
-  <div className={'Card'}>
-    <h1 className={'Card__title'}>Marco Bartelt</h1>
-    <p className={'Card__claim'}>Software Developer</p>
+  <div className="Card">
+    <h1 className="Card__title">Marco Bartelt</h1>
+    <p className="Card__claim">Software Engineer</p>
     <Divider />
-    <ul className={'Card__list'}>
+    <ul className="Card__list">
       <li>
         <Icon>
           <Envelope />
         </Icon>
-        <a href={'mailto:marco.bartelt94@gmail.com'}>marco.bartelt94@gmail.com</a>
+        <a href="mailto:marco.bartelt94@gmail.com">marco.bartelt94@gmail.com</a>
       </li>
       <li>
         <Icon>
           <Code />
         </Icon>
-        <a href={'https://www.pro-volution.de'} target={'_blank'}>pro.volution GmbH</a>
+        <a href="https://www.pro-volution.de"
+           target="_blank"
+           rel="noopener noreferrer"
+           aria-label="Current employer"
+        >
+          pro.volution GmbH
+        </a>
       </li>
       <li>
         <Icon>
@@ -34,13 +40,21 @@ const Card = () => (
         Düsseldorf
       </li>
     </ul>
-    <div className={'Card__footer'}>
-      <a href={'https://github.com/GoodGuyMarco'} target={'_blank'}>
+    <div className="Card__footer">
+      <a href="https://github.com/GoodGuyMarco"
+         target="_blank"
+         rel="noopener noreferrer"
+         aria-label="My GitHub profile"
+      >
         <Icon>
           <GitHub />
         </Icon>
       </a>
-      <a href={'https://www.xing.com/profile/Marco_Bartelt2'} target={'_blank'}>
+      <a href="https://www.xing.com/profile/Marco_Bartelt2"
+         target="_blank"
+         rel="noopener noreferrer"
+         aria-label="My Xing profile"
+      >
         <Icon>
           <Xing />
         </Icon>
