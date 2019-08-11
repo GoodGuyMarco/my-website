fe4f0f0d1e26793c994a3d6b3d4a71a8af335701
Marco Bartelt <mbartelt@pro-volution.de>
2019-08-11 16:05:20 +0200

    adds create react app, makes everything work again

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Card from './Components/Card';
+import './App.scss';
+
+const App = () => <Card />;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-/* @flow */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Portfolio from './Components/Portfolio';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Portfolio />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
