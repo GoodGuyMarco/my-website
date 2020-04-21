082c7c95b9492112d095f73af4ae5efc59a91a4f
Marco Bartelt <mbartelt@pro-volution.de>
2020-04-21 08:32:32 +0200

    adds a proper styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Not found" />
+    <section className="h-full flex flex-col justify-center items-center">
+      <h1 className="text-4xl">
+        What are <span className="font-semibold text-blue-600">you</span> looking for?
+      </h1>
+      <p className="mb-4">You just hit a page that does not exist... the sadness.</p>
+      <Link
+        to="/"
+        className="bg-white py-2 px-4 border border-blue-600 text-blue-600 font-semibold rounded cursor-pointer transition duration-200 transform hover:scale-105"
+      >
+        Take me back
+      </Link>
+    </section>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
